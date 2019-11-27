10c414591ecc8160621d20a6ba69cb1f8dc613f0
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2019-11-27 14:51:30 +0100

    poczatek gui

--- PYTHON/newMain.py
+++ PYTHON/newMain.py
@@ -5,15 +5,32 @@
 # Created by: PyQt5 UI code generator 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
+import serial
+import time
+import statistics
+import math
+import matplotlib.pyplot as plt
+import csv
+import os
+
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QApplication
+import sys
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5 import QtGui, QtCore, QtWidgets
 
 
-from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):
+class Ui_MainWindow(QMainWindow):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(450, 340)
+        
+        
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
@@ -29,6 +46,8 @@ class Ui_MainWindow(object):
         self.openButton.setObjectName("openButton")
         self.gridLayout.addWidget(self.openButton, 2, 0, 1, 1)
 
+        
+
         self.saveButton = QtWidgets.QPushButton(self.gridLayoutWidget)
         self.saveButton.setEnabled(False)
         self.saveButton.setObjectName("saveButton")
@@ -39,7 +58,7 @@ class Ui_MainWindow(object):
         self.gridLayout.addWidget(self.exitButton, 6, 0, 1, 1)
 
         self.startButton = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.startButton.setEnabled(False)
+        #self.startButton.setEnabled(False)
         self.startButton.setObjectName("startButton")
         self.gridLayout.addWidget(self.startButton, 1, 0, 1, 1)
 
@@ -73,7 +92,7 @@ class Ui_MainWindow(object):
         self.progressBar.setTextDirection(QtWidgets.QProgressBar.TopToBottom)
         self.progressBar.setObjectName("progressBar")
         self.verticalLayout.addWidget(self.progressBar)
-        
+
         self.gridLayout.addLayout(self.verticalLayout, 1, 1, 1, 1)
 
         self.analyzeButton = QtWidgets.QPushButton(self.gridLayoutWidget)
@@ -110,6 +129,22 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.setupLater()
+
+    def setupLater(self):
+        print("setupLater ... ")
+        self.filename = ''
+        
+        self.openButton.clicked.connect(lambda: self.openFile())
+        self.saveButton.clicked.connect(lambda: self.saveFile())
+        self.exitButton.clicked.connect(lambda: self.exit())
+        self.analyzeButton.clicked.connect(lambda: self.analyze())
+        self.connectButton.clicked.connect(lambda: self.connect())
+        self.startButton.clicked.connect(lambda: self.start())
+        
+        
+
+        
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -128,6 +163,138 @@ class Ui_MainWindow(object):
         self.connectButton.setText(_translate("MainWindow", "Connect to Arduino"))
         self.connectLabel.setText(_translate("MainWindow", "Ready..."))
 
+
+    def exit(self):
+        print("exiting...")
+
+    def analyze(self):
+        print("analyze...")
+
+    def analyze(self):
+        print("starting...")
+        self.startButton.setEnabled(False)
+        seq = []
+        count = 1
+
+        if self.radio1.isChecked : measurements = 1
+        elif self.radio2.isChecked : measurements = 2
+        elif self.radio3.isChecked : measurements = 3
+
+        if measurements == '2':
+            ser.write(bytes(measurements, 'utf-8'))
+        elif measurements == '3':
+            ser.write(bytes(measurements, 'utf-8'))
+        else:
+            measurements = '1'
+            ser.write(bytes(measurements, 'utf-8'))
+
+        measurements = int(measurements)
+
+        m = [180]
+
+        time.sleep(3)
+        while True:
+            for c in ser.read():
+                if chr(c) == '\n':
+                    data = joined_seq.split(",")
+                    try:
+                        # print(str(data))
+                        # print(str(data.__len__()))
+                        if data.__len__() < 2 :
+                            break
+                        angle = int(data[0])
+                        distance = []
+                        i = 0
+                        while i < measurements:
+                            distance.append(int(data[i+1]) / 58.2)
+                            i += 1
+                        print("angle: " + str(angle) + "; " + str(distance))
+                        m.insert(angle, statistics.median(distance))
+                        if angle == 180:
+                            print("creating graph ... ")
+                            create_graph(m)
+                            save(m)
+                            print("done!")
+                    except ValueError:
+                        print(joined_seq)
+                    except IndexError:
+                        print(joined_seq)
+
+                    seq = []
+                    count += 1
+                    break
+                seq.append(chr(c))  # convert from ANSII
+                joined_seq = ''.join(str(v) for v in seq)  # Make a string from arrays
+
+
+        ser.close()
+
+
+
+    def connect(self):
+        print("connect...")
+        self.connectButton.setEnabled(False)
+        self.connectButton.setText("Connecting...")
+
+        try:
+            self.ser = serial.Serial(
+            port='COM4',\
+            baudrate=9600,\
+            parity=serial.PARITY_NONE,\
+            stopbits=serial.STOPBITS_ONE,\
+            bytesize=serial.EIGHTBITS,\
+                timeout=0)
+
+            time.sleep(3)
+            self.connectLabel.setText("connected to : " + self.ser.portstr)
+            self.connectButton.setText("Reconnect")
+        except:
+            self.showdialog("error", "couldnt connect")
+
+
+        
+
+        self.connectButton.setEnabled(True)
+
+        
+    def showdialog(self, title, message):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setText(message)
+        msg.setWindowTitle(title)
+        msg.setStandardButtons(QMessageBox.Ok)
+        #msg.buttonClicked.connect(lambda button: button.text())
+        retval = msg.exec_()
+        print("value of pressed message box button:", retval)
+        return retval
+    
+    def saveFile(self):
+        print("saving file ...")
+        # bedzie zapisywal pomiary do pliku csv
+
+
+        #  # Only open dialog if there is no filename yet
+        # # PYQT5 Returns a tuple in PyQt5, we only need the filename
+        # if not self.filename:
+        #     self.filename = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File')[0]
+
+        # if self.filename:
+        #     # We just store the contents of the text file along with the
+        #     # format in html, which Qt does in a very nice way for us
+        #     with open(self.filename, "wt") as file:
+        #         file.write(self.text.toPlainText())
+
+        #     self.changesSaved = True
+
+
+
+    def openFile(self):
+        print("opening file...")
+        self.filename = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', ".", "(*.csv)")[0]
+
+        if self.filename:
+            with open(self.filename, "rt") as file:
+                self.openLabel.setText("opened: " + self.filename)
 
 if __name__ == "__main__":
     import sys
