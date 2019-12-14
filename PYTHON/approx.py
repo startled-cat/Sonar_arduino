72c7bb2c144e40063c414777e6367712d52f7ccc
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2019-12-14 14:42:45 +0100

    approx in seperate file

--- PYTHON/approx.py
+++ PYTHON/approx.py
@@ -1,3 +1,20 @@
+import math
+
 def approx(data, radius):
-    print(data)
+    x, y = toXY(data)
+
+    #write your code here
+
+    print(x)
+    print(y)
     print(radius)
+
+def toXY(data):
+    y = []
+    x = []
+    i = 0
+    while i < len(data) :
+        y.append(math.sin(math.radians(i)) * (data.get(i)))
+        x.append(math.cos(math.radians(i)) * (data.get(i)))
+        i += 1
+    return x, y
